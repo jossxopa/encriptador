0d107b6035c7b4d8d9093e7273333bd5a802d007
jossxopa <juanjosexopa@gmail.com>
2023-05-22 03:18:15 -0600

    conclusión de lógica

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
   let textoEncriptador = document.getElementById("resultado-consola");
   let botonEncriptar = document.getElementById("boton-encriptar");
   let botonDesencriptar = document.getElementById("boton-desencriptar");
-  let botonCopiar = document.getElementById("boton-copiar");*/
+  let botonCopiar = document.getElementById("boton-copiar");
+
+  let abrirModal = document.getElementById("boton-encriptar");
+  let cerrarModal = document.getElementById("btn-cerrarModal");
+  let modal = document.getElementById("modal"); */
 
   function encriptarTexto () {
     let sustituciones = [
@@ -16,18 +20,34 @@ document.addEventListener('DOMContentLoaded', function() {
   
     let textoUsuario = document.getElementById("input-original").value;
     let textoPorEncriptar = textoUsuario;
+
+    if (/^[a-z\s]*$/.test(textoUsuario)) {
+    } else {
+      let modal = document.getElementById("modal");
+      modal.showModal(); 
+      return;
+      /*alert("No se puede utilizar acentos, mayúsculas ni ningún tipo de signo")
+      return;*/
+    };
+ 
     for (const sustitucion of sustituciones) { // We use for of loop for arrays. It is very hand way to iterate through an array if we are not interested in the index of each element in the array.//
       const letra = sustitucion[0]; // es cero porque toma en cuenta los elementos de las vocales, o sea las vocales toman el lugar de 0//
       const sustituta = sustitucion[1]; // es 1 porque 1 es el lugar del indice de las palabras que van a sustituir a 0 //
       textoPorEncriptar = textoPorEncriptar.replace(new RegExp(letra, "img"), sustituta);
     }
- 
+
     document.getElementById("resultado-consola").value = textoPorEncriptar;
     
   }
 
   let botonEncriptar = document.getElementById("boton-encriptar");
   botonEncriptar.addEventListener("click", encriptarTexto);
+
+  let cerrarModal = document.getElementById("btn-cerrarModal");
+  cerrarModal.addEventListener("click", ()=> {
+    let modal = document.getElementById("modal");
+    modal.close();
+  })
 
   // Desencriptar 
 
@@ -42,6 +62,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
     let textoConsola = document.getElementById("input-original").value;
     let textoPorDesencriptar = textoConsola;
+
     for (let i = sustituciones.length -1; i >=0; i--) {
       const sustitucion = sustituciones [i]; 
       const secuenciaEncriptada = sustitucion[0];
@@ -56,26 +77,16 @@ document.addEventListener('DOMContentLoaded', function() {
   let botonDesencriptar = document.getElementById("boton-desencriptar");
   botonDesencriptar.addEventListener("click", desencriptarTexto);
 
-})
-  
-  
-  /*function  () {
-    let textoEncriptado = document.getElementById("resultado-consola");
-    const letrasSustituidas = [ ["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"] ];
-  
-    encriptarTexto("resultado-consola");
-    
-  }
-  
-  function textCopy () {
-    let textoUsuario = document.getElementById("#input-original").value;
-    const letrasSustituidas = [ ["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"] ];
-  
-    encriptarTexto("resultado-consola");
-    
-  }
-  
-  
-    
 
-});*/
+})
+
+/* Copiar texto */ 
+
+function copiarTexto () {
+  let copyText = document.getElementById("resultado-consola");
+  copyText.select();
+
+  document.execCommand("copy");
+    alert("Texto copiado");
+  
+}
